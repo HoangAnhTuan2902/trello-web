24a685838d652fdc23a74f511b1845e3fd3e00c0
HoangAnhTuan2902 <hoanganhtuan29022004@gmail.com>
2024-08-29 14:16:09 +0700

    dark_light_p1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import Button from '@mui/material/Button';
+import { useColorScheme } from '@mui/material/styles';
+
+function ModeToggle() {
+	const { mode, setMode } = useColorScheme();
+	return (
+		<Button
+			onClick={() => {
+				setMode(mode === 'light' ? 'dark' : 'light');
+			}}>
+			{mode === 'light' ? 'Turn dark' : 'Turn light'}
+		</Button>
+	);
+}
 
 function App() {
 	return (
 		<>
+			<ModeToggle />
 			<Button variant='text'>Text</Button>
 			<Button variant='contained'>Contained</Button>
 			<Button variant='outlined'>Outlined</Button>
