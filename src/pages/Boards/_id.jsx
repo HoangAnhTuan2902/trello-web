75cae0704194866b36565bddfe6d7445f383b753
HoangAnhTuan2902 <hoanganhtuan29022004@gmail.com>
2024-09-05 22:10:07 +0700

    add new column and card p2

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,12 +1,18 @@
 // Board Details
 import Container from '@mui/material/Container';
+import { isEmpty } from 'lodash';
 
 import AppBar from '~/components/AppBar/AppBar';
 import BoardBar from './BoardBar/BoardBar';
 import BoardContent from './BoardContent/BoardContent';
-import { mockData } from '~/apis/mock-data';
+// import { mockData } from '~/apis/mock-data';
 import { useEffect, useState } from 'react';
-import { fetchBoardDetailsAPI } from '~/apis';
+import {
+	createNewCardAPI,
+	createNewColumnAPI,
+	fetchBoardDetailsAPI,
+} from '~/apis';
+import { generatePlaceholderCard } from '~/utils/formatters';
 
 function Board() {
 	const [board, setBoard] = useState(null);
@@ -14,9 +20,47 @@ function Board() {
 	useEffect(() => {
 		const boardId = '66d7cfc6633d62486c60be2a';
 		fetchBoardDetailsAPI(boardId).then((board) => {
+			board.columns.forEach((column) => {
+				if (isEmpty(column.cards)) {
+					column.cards = [generatePlaceholderCard(column)];
+					column.cardOrderIds = [column.cards[0]._id];
+				}
+			});
 			setBoard(board);
 		});
 	}, []);
+
+	const createNewColumn = async (newColumnData) => {
+		const createdColumn = await createNewColumnAPI({
+			...newColumnData,
+			boardId: board._id,
+		});
+
+		// xử lý kéo thả card vào 1 column rỗng
+		createdColumn.cards = [generatePlaceholderCard(createdColumn)];
+		createdColumn.cardOrderIds = [createdColumn.cards[0]._id];
+
+		const newBoard = { ...board };
+		newBoard.columns.push(createdColumn);
+		newBoard.columnOrderIds.push(createdColumn._id);
+		setBoard(newBoard);
+	};
+
+	const createNewCard = async (newCardData) => {
+		const createdCard = await createNewCardAPI({
+			...newCardData,
+			boardId: board._id,
+		});
+		const newBoard = { ...board };
+		const columnToUpdate = newBoard.columns.find(
+			(column) => column._id === createdCard.columnId,
+		);
+		if (columnToUpdate) {
+			columnToUpdate.cards.push(createdCard);
+			columnToUpdate.cardOrderIds.push(createdCard._id);
+		}
+		setBoard(newBoard);
+	};
 
 	return (
 		<Container
@@ -25,7 +69,11 @@ function Board() {
 			sx={{ height: '100vh' }}>
 			<AppBar />
 			<BoardBar board={board} />
-			<BoardContent board={board} />
+			<BoardContent
+				createNewCard={createNewCard}
+				createNewColumn={createNewColumn}
+				board={board}
+			/>
 		</Container>
 	);
 }
