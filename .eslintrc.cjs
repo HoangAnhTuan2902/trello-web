a6f106e3c3780731dd317ce0b70186048e4005c2
HoangAnhTuan2902 <hoanganhtuan29022004@gmail.com>
2024-09-09 00:06:50 +0700

    create interceptors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -28,6 +28,7 @@ module.exports = {
 		],
 
 		//common
+
 		'no-useless-catch': 0,
 		'no-console': 1,
 		'no-extra-boolean-cast': 0,
--- src/apis/index.js
+++ src/apis/index.js
@@ -1,43 +1,40 @@
-import axios from 'axios';
-import { API_ROOT } from '~/utils/constants';
+import axios from '~/apis/axiosConfig';
+// import { API_ROOT } from '~/utils/constants';
 
 /** Board */
 export const fetchBoardDetailsAPI = async (boardId) => {
-	const res = await axios.get(`${API_ROOT}/v1/boards/${boardId}`);
-	return res.data;
+	const res = await axios.get(`/v1/boards/${boardId}`);
+	return res;
 };
 
 export const updateBoardDetailsAPI = async (boardId, updateData) => {
-	const res = await axios.put(`${API_ROOT}/v1/boards/${boardId}`, updateData);
-	return res.data;
+	const res = await axios.put(`/v1/boards/${boardId}`, updateData);
+	return res;
 };
 
 /** Column */
 export const createNewColumnAPI = async (newColumnData) => {
-	const res = await axios.post(`${API_ROOT}/v1/columns`, newColumnData);
-	return res.data;
+	const res = await axios.post(`/v1/columns`, newColumnData);
+	return res;
 };
 
 export const moveCardToDifferentColumnAPI = async (updateData) => {
-	const res = await axios.put(
-		`${API_ROOT}/v1/boards/supports/moving_card`,
-		updateData,
-	);
-	return res.data;
+	const res = await axios.put(`/v1/boards/supports/moving_card`, updateData);
+	return res;
 };
 
 export const updateColumnDetailsAPI = async (ColumnId, updateData) => {
-	const res = await axios.put(`${API_ROOT}/v1/columns/${ColumnId}`, updateData);
-	return res.data;
+	const res = await axios.put(`/v1/columns/${ColumnId}`, updateData);
+	return res;
 };
 
 export const deleteColumnDetailsAPI = async (ColumnId) => {
-	const res = await axios.delete(`${API_ROOT}/v1/columns/${ColumnId}`);
-	return res.data;
+	const res = await axios.delete(`/v1/columns/${ColumnId}`);
+	return res;
 };
 
 /** Card */
 export const createNewCardAPI = async (newCardData) => {
-	const res = await axios.post(`${API_ROOT}/v1/cards`, newCardData);
-	return res.data;
+	const res = await axios.post(`/v1/cards`, newCardData);
+	return res;
 };
